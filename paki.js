d335b2844089aebbeca785bab914a6879ac2f8a1
bohandre <bohandre24@gmail.com>
2021-11-18 16:44:55 -0500

    add new repository

---
+++ paki.js
@@ -0,0 +1,13 @@
+var imagenes = [];
+imagenes["Cauchin"] = "Cauchin.png";
+imagenes["Pokacho"] = "Pokacho.png";
+imagenes["Tocinauro"] = "Tocinauro.png";
+
+var cauchin = new Pakiman("Cauchin", 100, 30);
+var pokacho = new Pakiman("Pokacho", 80, 50);
+var tocinauro = new Pakiman("Tocinauro", 120, 40);
+
+cauchin.mostrar();
+pokacho.mostrar();
+tocinauro.mostrar();
+
